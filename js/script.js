a038abe762ffccd666a49465747af8bb999f8923
Joy Naeku <naekujoy@gmail.com>
2017-12-11 08:28:47 +0300

    Added business logic part for the javascript code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-//
+//business logic
+var userNumber = [];
+function pingPong (number){
+    for (var index = 1; index <= number; index+=1){
+        if(index % 15 === 0){
+            userNumber.push("pingpong");
+        }
+        else if(index % 5 === 0){
+            userNumber.push("pong");
+        }
+        else if(index % 3 === 0){
+            userNumber.push("ping");
+        }
+        else{
+            userNumber.push(index);
+        }
+    }
+}
